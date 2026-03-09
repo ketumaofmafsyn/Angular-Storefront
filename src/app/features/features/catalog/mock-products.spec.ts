9482bb46c4c42db9b865b5ecd33669ae4de585cc
Moloto Jansen Malema <135948859+ketumaofmafsyn@users.noreply.github.com>
2026-03-09 15:36:04 +0200

    Initial commit

--- src/app/features/features/catalog/mock-products.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MockProducts } from './mock-products.ts';
-
-describe('MockProducts', () => {
-  it('should create an instance', () => {
-    expect(new MockProducts()).toBeTruthy();
-  });
-});
